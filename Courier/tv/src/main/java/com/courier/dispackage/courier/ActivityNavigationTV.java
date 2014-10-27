a16180b708cdde388f4408986db002b3612ea7f3
gutlessVADER <matthewecker@MLG-420-W33DSC0P3-MOUNTAIN-DEW-DORITOS-1V1-ME-RUST-FAGGOT.local>
2014-10-27 16:17:01 -0500

    Initial Project Generated by Android Studio
    Nothing has been done post-initialization, menu setup selected.

---
+++ Courier/tv/src/main/java/com/courier/dispackage/courier/ActivityNavigationTV.java
@@ -0,0 +1,13 @@
+package com.courier.dispackage.courier;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ActivityNavigationTV extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_activity_navigation_tv);
+    }
+}
